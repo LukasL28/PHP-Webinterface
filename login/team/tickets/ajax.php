262ba25646386ad75bb24f2daec5d370a04feb30
Lukas <57566886+LukasL28@users.noreply.github.com>
2022-03-23 17:34:41 +0100

    Delete login/team/tickets directory

--- login/team/tickets/ajax.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("datamanager.php");
-if(isset($_GET["del"]) && isset($_POST["name"])){
-  //Delete an account by name
-  deleteAccount($_POST["name"]);
-} else {
-  echo "Invalid request.";
-}
- ?>
